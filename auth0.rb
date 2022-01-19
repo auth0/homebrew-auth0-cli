34b1ef2538dd0a8c61dbb6fa8ad77b7ce522e1fd
auth0 <support@auth0.com>
2022-01-19 14:57:31 -0500

    Brew formula update for auth0-cli version v0.11.4

--- auth0.rb
+++ auth0.rb
@@ -5,13 +5,13 @@
 class Auth0 < Formula
   desc "Auth0 command-line tool to supercharge your developer workflow"
   homepage "https://auth0.github.io/auth0-cli"
-  version "0.11.3"
+  version "0.11.4"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/auth0/auth0-cli/releases/download/v0.11.3/auth0-cli_0.11.3_Darwin_arm64.tar.gz"
-      sha256 "bec5db2ed0f0a48b73ad868175ede1163bf10349129dcd3bf6706ad660e56f15"
+      url "https://github.com/auth0/auth0-cli/releases/download/v0.11.4/auth0-cli_0.11.4_Darwin_arm64.tar.gz"
+      sha256 "850b6370ae1425094a66dbff6fcdeff4a21c6a4ed55383daa659485d5f7053ba"
 
       def install
         bin.install "auth0"
@@ -22,8 +22,8 @@ class Auth0 < Formula
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/auth0/auth0-cli/releases/download/v0.11.3/auth0-cli_0.11.3_Darwin_x86_64.tar.gz"
-      sha256 "53f093df7d505de640b86f6cbaa423822f86be09f21c0e2c05c585433211b292"
+      url "https://github.com/auth0/auth0-cli/releases/download/v0.11.4/auth0-cli_0.11.4_Darwin_x86_64.tar.gz"
+      sha256 "6968ade7fa55fe355e04650bc8905e38dcb078942d6db19c27aeb4e8e1e587c1"
 
       def install
         bin.install "auth0"
@@ -36,9 +36,9 @@ class Auth0 < Formula
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/auth0/auth0-cli/releases/download/v0.11.3/auth0-cli_0.11.3_Linux_x86_64.tar.gz"
-      sha256 "15f21aa86e395cf74447a852a03fdc4c27dd03e26b8138d2c21664b3c4b53a26"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/auth0/auth0-cli/releases/download/v0.11.4/auth0-cli_0.11.4_Linux_arm64.tar.gz"
+      sha256 "bff5cb9f33a9c788f1a0b5ddae0e084773c1e95e448586c8dc6b59aee1458613"
 
       def install
         bin.install "auth0"
@@ -48,9 +48,9 @@ class Auth0 < Formula
         (zsh_completion/"_auth0").write `#{bin}/auth0 completion zsh`
       end
     end
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/auth0/auth0-cli/releases/download/v0.11.3/auth0-cli_0.11.3_Linux_arm64.tar.gz"
-      sha256 "7ade16fbc170ce64c0a6072b367f51d01f26307f9136e6e725ba39b742d27029"
+    if Hardware::CPU.intel?
+      url "https://github.com/auth0/auth0-cli/releases/download/v0.11.4/auth0-cli_0.11.4_Linux_x86_64.tar.gz"
+      sha256 "bfdaa15aa85599116e191cfcadd9a1f357c1af004b9025319fa21a3f9a14494e"
 
       def install
         bin.install "auth0"
